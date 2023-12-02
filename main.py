f2248b1a011df1ffe204890147c97eb4383a8104
Noccss <nicolas.olicarvalho@gmail.com>
2023-12-02 17:06:00 -0300

    Validate movimentation horizontal, vertical and 3x3 grid

--- main.py
+++ main.py
@@ -13,15 +13,38 @@ grid = [
 class SudokuValidate:
     @staticmethod
     def verify_repeated_numbers(grid):
+        def print_error(position_error, repeated_number, direction, quantity_error):
+            print(False)
+            print("Explanation:")
+            print(f"There does not exist a valid Sudoku for the input grid, since there are {quantity_error} {repeated_number}s in the {position_error + 1} {direction}.\nWhich cannot be replaced.")
+
         for row in range(len(grid)):
             for num in range(1, len(grid) + 1):
                 if grid[row].count(num) > 1:
-                    return True, row, num, 'row', grid[row].count(num)
+                    print_error(row, num, 'row', grid[row].count(num))
+                    return True
 
                 if [grid[j][row] for j in range(len(grid))].count(num) > 1:
-                    return True, row, num, 'column', [grid[j][row] for j in range(len(grid))].count(num)
-        return False, None, None, None, None
+                    print_error(row, num, 'column', [grid[j][row] for j in range(len(grid))].count(num))
+                    return True
+        return False
+    
+    @staticmethod
+    def validate_upright_horizontal_moviment(grid, row, column, num):
+        if num in grid[row]:
+            return False
+        
+        if num in [grid[i][column] for i in range(len(grid))]:
+            return False
+        
+        return True
+    
+    @staticmethod
+    def validate_subgrid_moviment(grid, row, column, num):
+        block_size = int(len(grid) ** 0.5)
+        block_start_row, block_start_col = block_size * (row // block_size), block_size * (column // block_size)
 
+        return not any(num == grid[block_start_row + i][block_start_col + j] for i in range(block_size) for j in range(block_size))
 
 class SudokuSolution:
     @staticmethod
@@ -37,19 +60,21 @@ class SudokuSolution:
         row, column = SudokuSolution.locate_blank_house(grid)
 
         if row is None and column is None:
-            print("Blank house is over")
             return True
         
+        for num in range(1, len(grid) + 1):
+            if SudokuValidate.validate_upright_horizontal_moviment(grid, row, column, num) and \
+                    SudokuValidate.validate_subgrid_moviment(grid, row, column, num):
+                grid[row][column] = num
+
+                if SudokuSolution.solve_sudoku(grid):
+                    return True
+                grid[row][column] = 0
+        
 def main():
-    has_repeated, position_error, repeated_number, direction, quantity_error = SudokuValidate.verify_repeated_numbers(grid)
 
-    if not has_repeated:
+    if not SudokuValidate.verify_repeated_numbers(grid):
         SudokuSolution.solve_sudoku(grid)
-    else:
-        print(has_repeated)
-        print("Explanation:")
-        print(f"There does not exist a valid Sudoku for the input grid, since there are {quantity_error} {repeated_number}s in the {position_error + 1} {direction}.\nWhich cannot be replaced.")
-
         
 if __name__ == "__main__":
     main()
